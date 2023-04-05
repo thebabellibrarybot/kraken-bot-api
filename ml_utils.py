e438dde35aa037ddb0d1750aa448921691e12e5b
thebabellibrarybot <jack@thebabellibrary.org>
2023-04-04 21:35:30 -0400

    kraken-bot:filter_less_ii

--- ml_utils.py
+++ ml_utils.py
@@ -3,6 +3,20 @@ from PIL import Image
 from kraken import pageseg
 from utils import filter_small_boxes
 import os
+import numpy as np
+from skimage import morphology
+
+def remove_connected_components(img, min_size=100):
+    # Convert the image to a binary numpy array
+    bin_img = np.array(img, dtype=np.bool)
+
+    # Remove small connected components using skimage
+    cleaned_img = morphology.remove_small_objects(bin_img, min_size=min_size)
+
+    # Convert the cleaned image back to PIL Image format
+    cleaned_img = Image.fromarray(np.uint8(cleaned_img * 255))
+
+    return cleaned_img
 
 # might add custom model path to this so diff models can be selected
 def segline_and_crop (img_path, fi_name = None, mime_type = None):
@@ -17,6 +31,7 @@ def segline_and_crop (img_path, fi_name = None, mime_type = None):
         im = Image.open(img_path)
         print(im, 'im')
         bw_im = binarization.nlbin(im)
+        bw_im = remove_connected_components(bw_im)
         seg = pageseg.segment(bw_im)
         print(seg, 'seg')
         print(seg['boxes'], 'seg boxes')
--- utils.py
+++ utils.py
@@ -5,57 +5,8 @@ def filter_small_boxes(bboxes):
     :param bboxes: A list of bounding boxes, each represented as a tuple of (xmin, ymin, xmax, ymax)
     :return: A list of filtered bounding boxes
     """
-    print('got to filter sml boxes')
     filtered_bboxes = []
-    #areas = []
-    #for bbox in bboxes:
-    #    print('bbox in fliter sm boxes', bbox)
-    #    area = (bbox[2] - bbox[0]) * (bbox[3] - bbox[1])
-    #    areas.append(area)
-
     for i, bbox in enumerate(bboxes):
-        #bbox_area = areas[i]
-        #is_small = False
-        for j, other_bbox in enumerate(bboxes):
-            #if i == j:
-            #    continue
-            #other_area = areas[j]
-            #if bbox_area <= other_area * 0.1:
-            #    is_small = True
-            #    break
-        #if not is_small:
-            filtered_bboxes.append(bbox)
+        filtered_bboxes.append(bbox)
 
     return filtered_bboxes
-    """
-    Crops the image for each bounding box and saves the cropped images to the 'DATA/output/' directory.
-
-    :param img: An already opened PIL image
-    :param bboxes: A list of bounding boxes, each represented as a tuple of (xmin, ymin, xmax, ymax)
-    :return: a dict with {file_name: ['img_1', 'img_2']}
-    """
-    # Create output directory if it does not exist
-    #outpath = os.getcwd()
-    if not os.path.exists('DATA/output/'):
-        os.makedirs('DATA/output/')
-
-    return_obj = {}
-
-    # Loop through each bounding box
-    for i, bbox in enumerate(bboxes):
-        # Get the coordinates of the bounding box
-        xmin, ymin, xmax, ymax = bbox
-
-        # Crop the image
-        cropped_img = img.crop((xmin, ymin, xmax, ymax))
-        # Save the cropped image to s3
-        ext = os.path.splitext(fi_name)[1]
-        crop_fi_name = fi_name.split(ext)[0] + '_' + str(i) + ext
-        s3url = upload_img_s3(cropped_img, crop_fi_name)
-
-        # return saved file dictionary
-        if fi_name not in return_obj:
-            return_obj[fi_name] = [s3url]
-        else: 
-            return_obj[fi_name].append(s3url)
-    return return_obj
